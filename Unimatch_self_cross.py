5949253b424b599bd02faa00cf4437fcf5ab0449
zerone-fg <52369263+zerone-fg@users.noreply.github.com>
2023-08-17 23:39:25 +0800

    Update Unimatch_self_cross.py

--- Unimatch_self_cross.py
+++ Unimatch_self_cross.py
@@ -130,56 +130,55 @@ def main():
 
             model.train()
 
-            # num_lb, num_ulb = img_x.shape[0], img_u_w.shape[0]
-
-            # pred_u_w, pred_u_w_fp = model(img_u_w, need_fp=True)
-            #
-            # pred_u_s1 = model(img_u_s1, cluster=True)
-            # pred_u_s3 = model(img_u_s1)
-            #
+            num_lb, num_ulb = img_x.shape[0], img_u_w.shape[0]
+            pred_u_w, pred_u_w_fp = model(img_u_w, need_fp=True)
+            
+            pred_u_s1 = model(img_u_s1, cluster=True)
+            pred_u_s3 = model(img_u_s1)
+            
             pred_x, pred_u_s2 = model(torch.cat((img_x, img_u_s2), dim=0), mask_x, cluster=True, cls_aug=True)
 
-            # pred_u_w = pred_u_w.detach()
-            # conf_u_w = pred_u_w.softmax(dim=1).max(dim=1)[0]
-            # mask_u_w = pred_u_w.argmax(dim=1)
-            #
-            # mask_u_w_cutmixed1, conf_u_w_cutmixed1 = mask_u_w.clone(), conf_u_w.clone()
-            # mask_u_w_cutmixed2, conf_u_w_cutmixed2 = mask_u_w.clone(), conf_u_w.clone()
-            #
-            # mask_u_w_cutmixed1[cutmix_box1 == 1] = mask_u_w_mix[cutmix_box1 == 1]
-            # conf_u_w_cutmixed1[cutmix_box1 == 1] = conf_u_w_mix[cutmix_box1 == 1]
-            #
-            # mask_u_w_cutmixed2[cutmix_box2 == 1] = mask_u_w_mix[cutmix_box2 == 1]
-            # conf_u_w_cutmixed2[cutmix_box2 == 1] = conf_u_w_mix[cutmix_box2 == 1]
+            pred_u_w = pred_u_w.detach()
+            conf_u_w = pred_u_w.softmax(dim=1).max(dim=1)[0]
+            mask_u_w = pred_u_w.argmax(dim=1)
+            
+            mask_u_w_cutmixed1, conf_u_w_cutmixed1 = mask_u_w.clone(), conf_u_w.clone()
+            mask_u_w_cutmixed2, conf_u_w_cutmixed2 = mask_u_w.clone(), conf_u_w.clone()
+            
+            mask_u_w_cutmixed1[cutmix_box1 == 1] = mask_u_w_mix[cutmix_box1 == 1]
+            conf_u_w_cutmixed1[cutmix_box1 == 1] = conf_u_w_mix[cutmix_box1 == 1]
+            
+            mask_u_w_cutmixed2[cutmix_box2 == 1] = mask_u_w_mix[cutmix_box2 == 1]
+            conf_u_w_cutmixed2[cutmix_box2 == 1] = conf_u_w_mix[cutmix_box2 == 1]
 
             loss_x = (criterion_ce(pred_x, mask_x) + criterion_dice(pred_x.softmax(dim=1),
                                                                     mask_x.unsqueeze(1).float())) / 2.0
 
-            # loss_u_s1 = criterion_dice(pred_u_s1.softmax(dim=1), mask_u_w_cutmixed1.unsqueeze(1).float(),
-            #                            ignore=(conf_u_w_cutmixed1 < cfg['conf_thresh']).float())
-            #
-            # loss_u_s2 = criterion_dice(pred_u_s2.softmax(dim=1), mask_u_w_cutmixed2.unsqueeze(1).float(),
-            #                            ignore=(conf_u_w_cutmixed2 < cfg['conf_thresh']).float())
-            #
-            # loss_u_s3 = criterion_dice(pred_u_s3.softmax(dim=1), mask_u_w_cutmixed1.unsqueeze(1).float(),
-            #                            ignore=(conf_u_w_cutmixed1 < cfg['conf_thresh']).float())
-            #
-            # loss_u_w_fp = criterion_dice(pred_u_w_fp.softmax(dim=1), mask_u_w.unsqueeze(1).float(),
-            #                              ignore=(conf_u_w < cfg['conf_thresh']).float())
+            loss_u_s1 = criterion_dice(pred_u_s1.softmax(dim=1), mask_u_w_cutmixed1.unsqueeze(1).float(),
+                                        ignore=(conf_u_w_cutmixed1 < cfg['conf_thresh']).float())
+            
+            loss_u_s2 = criterion_dice(pred_u_s2.softmax(dim=1), mask_u_w_cutmixed2.unsqueeze(1).float(),
+                                        ignore=(conf_u_w_cutmixed2 < cfg['conf_thresh']).float())
+            
+            loss_u_s3 = criterion_dice(pred_u_s3.softmax(dim=1), mask_u_w_cutmixed1.unsqueeze(1).float(),
+                                        ignore=(conf_u_w_cutmixed1 < cfg['conf_thresh']).float())
+            
+            loss_u_w_fp = criterion_dice(pred_u_w_fp.softmax(dim=1), mask_u_w.unsqueeze(1).float(),
+                                          ignore=(conf_u_w < cfg['conf_thresh']).float())
 
-            # loss = (loss_x + loss_u_s1 * 0.25 + loss_u_s2 * 0.25 + loss_u_w_fp * 0.5 + loss_u_s3 * 0.25) / 2.0
-            loss = loss_x
+            loss = (loss_x + loss_u_s1 * 0.25 + loss_u_s2 * 0.25 + loss_u_w_fp * 0.5 + loss_u_s3 * 0.25) / 2.0
+            
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             total_loss.update(loss.item())
             total_loss_x.update(loss_x.item())
-            # total_loss_s.update((loss_u_s1.item() + loss_u_s2.item() + loss_u_s3.item()) / 3.0)
-            # total_loss_w_fp.update(loss_u_w_fp.item())
-            #
-            # mask_ratio = (conf_u_w >= cfg['conf_thresh']).sum() / conf_u_w.numel()
-            # total_mask_ratio.update(mask_ratio.item())
+            total_loss_s.update((loss_u_s1.item() + loss_u_s2.item() + loss_u_s3.item()) / 3.0)
+            total_loss_w_fp.update(loss_u_w_fp.item())
+            
+            mask_ratio = (conf_u_w >= cfg['conf_thresh']).sum() / conf_u_w.numel()
+            total_mask_ratio.update(mask_ratio.item())
 
             iters = epoch * len(trainloader_u) + i
             lr = cfg['lr'] * (1 - iters / total_iters) ** 0.9
@@ -188,9 +187,9 @@ def main():
             if rank == 0:
                 writer.add_scalar('train/loss_all', loss.item(), iters)
                 writer.add_scalar('train/loss_x', loss_x.item(), iters)
-                # writer.add_scalar('train/loss_s', (loss_u_s1.item() + loss_u_s2.item() + loss_u_s3.item()) / 3.0, iters)
-                # writer.add_scalar('train/loss_w_fp', loss_u_w_fp.item(), iters)
-                # writer.add_scalar('train/mask_ratio', mask_ratio, iters)
+                writer.add_scalar('train/loss_s', (loss_u_s1.item() + loss_u_s2.item() + loss_u_s3.item()) / 3.0, iters)
+                writer.add_scalar('train/loss_w_fp', loss_u_w_fp.item(), iters)
+                writer.add_scalar('train/mask_ratio', mask_ratio, iters)
 
             if (i % (len(trainloader_u) // 8) == 0) and (rank == 0):
                 logger.info(
